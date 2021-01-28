a285fcaab75bc2880b6c024176353bba2525dcca
Emanuele Pace <emanuele.10@outlook.it>
2021-01-28 09:46:25 +0000

    First part completed

--- newFile.js
+++ newFile.js
@@ -1,7 +1,13 @@
 function leapYear(year){
     if (year % 400 === 0){
         return true
-    }
-}
+    } else if (year % 100 === 0 && year % 400 !== 0){
+        return false
+    } else if (year % 4 === 0 && year % 100 !== 0){
+        return true
+    } else if (year % 4 !== 0){
+        return false
+    };
+};
 
 module.exports = leapYear;
--- newFile.test.js
+++ newFile.test.js
@@ -7,4 +7,14 @@ describe("Leap Year", () => {
     it('Return true if year is divisible by 400', () => {
         expect(leapYear(1600)).toBe(true)
     });
+    it('Return false if year is divisible by 100 but not by 400', () => {
+        expect(leapYear(1700)).toBe(false)
+    });
+    it('Return true if year is divisible by 4 and not by 100', () => {
+        expect(leapYear(2004)).toBe(true)
+    });
+    it('Return false if year is not divisible by 4', () =>{
+        expect(leapYear(2009)).toBe(false)
+    });
 })
+// All years not divisible by 4 are NOT leap years 2009
